ef006d7c0314412bad32c80b2113b4f8b5af4ce1
Lea3D <47499451+Lea3D@users.noreply.github.com>
2024-09-10 20:23:14 +0200

    clean up

--- start.bat
+++ start.bat
@@ -10,10 +10,6 @@ C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command "Set-Executio
 :: Führe frontend.ps1 ohne Konsolenfenster aus
 start C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0frontend.ps1"
 
-######################↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑####################################
-# "-WindowStyle Hidden" in die Starparameter einfügen, um Konsolenfanster zu unterdrücken. #
-############################################################################################
-
 :: Setze die ursprüngliche Ausführungsrichtlinie zurück
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -command "Set-ExecutionPolicy %oldPolicy% -Scope CurrentUser -Force"
 
